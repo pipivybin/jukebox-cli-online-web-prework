b9098ef57e77c3669af027d8ea5a4771dbf6931f
Ming Duan <11203duan@gmail.com>
2019-03-04 16:09:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -43,29 +43,29 @@ end
 
 def play(songs)
 
-  # arr = (1..9).to_a
+  arr = (1..9).to_a
 
   puts "Please enter a song name or number:"
 
   input = gets.strip.chomp
 
-  # if arr.include?(input.to_i)
-  #   puts "Playing #{songs[input.to_i-1]}"
-  #
-  # elsif songs.include?(input)
-  #   puts "Playing #{input}"
-  #
-  # else
-  #   puts "Invalid input, please try again"
-
-  if  songs.include?(input)
-    puts "Playing #{input}"
-
-  elsif songs[input.to_i-1]
+  if arr.include?(input.to_i)
     puts "Playing #{songs[input.to_i-1]}"
 
-  else puts "Invalid input, please try again"
+  elsif songs.include?(input)
+    puts "Playing #{input}"
 
+  else
+    puts "Invalid input, please try again"
+
+  # if  songs.include?(input)
+  #   puts "Playing #{input}"
+  #
+  # elsif songs[input.to_i-1]
+  #   puts "Playing #{songs[input.to_i-1]}"
+  #
+  # else puts "Invalid input, please try again"
+  #
   end
 end
 
